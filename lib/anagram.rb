f9a569ce3f95d6be44f0afa4aab417d953894fb5
Madeline Stark <madeline.stark1@gmail.com>
2018-04-12 23:25:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(possibles)
+  possibles.each do |string|
+    anagrams = []
+    letters = string.split("")
+    wordy_letters = word.split("")
+    binding.pry
+    if letters.sort == wordy_letters.sort
+      anagram = letters.join
+      anagrams << anagram
+    end
+    anagrams
+  end
+end
+
+end
